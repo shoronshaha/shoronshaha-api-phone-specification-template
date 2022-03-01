68dee60af361295ae441da09f4f26b8a50a6cbb4
shoronshaha <smaranshaha@gmail.com>
2022-03-01 22:19:56 +0600

    style updated

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ const displayPhoneDetail = specifications => {
 
        <div class="flex flex-col md:flex-row md:max-w-full rounded-lg bg-white shadow-lg bg-teal-100 ">
                    
-               <img class=" w-full h-96 md:h-auto object-cover md:w-full rounded-t-lg md:rounded-none md:rounded-l-lg"
+               <img class="w-full"
                    src="${specifications.image}" alt="" />
                <div class="p-6 flex flex-col justify-start">
                     <h4 class="text-gray-900 text-3xl font-medium mb-2">
