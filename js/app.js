9a26348951a3a00dbf36562fcb6bd5abc0281f61
shoronshaha <smaranshaha@gmail.com>
2022-03-02 13:33:28 +0600

    final update

--- js/app.js
+++ js/app.js
@@ -12,6 +12,11 @@ const searchPhone = () => {
     // clear data
     searchField.value = '';
 
+    const phoneDetail = document.getElementById('phone-details');
+
+    // replace the previous specification to new one
+    phoneDetail.textContent = '';
+
     document.getElementById('error-message').style.display = 'none';
     document.getElementById('error2-message').style.display = 'none';
 
@@ -26,15 +31,8 @@ const searchPhone = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         fetch(url)
             .then(res => res.json())
-            .then(phone => displayPhone(phone.data))
-            .catch(error => displayError(error))
+            .then(phone => displayPhone(phone.data));
     }
-}
-
-// error handeler function
-const displayError = error => {
-    console.log(error);
-    document.getElementById('error2-message').style.display = 'block';
 }
 
 // display search results phone details 
@@ -46,10 +44,15 @@ const displayPhone = phones => {
     // searchResult clean;
     searchResult.textContent = '';
 
+    // error handeler function
+    if (phones == '') {
+        document.getElementById('error2-message').style.display = 'block';
+    }
+
     first20Phones.forEach(phone => {
         const div = document.createElement('div');
         div.innerHTML = `
-                <div class="rounded-lg shadow-lg bg-white max-w-sm text-center bg-teal-200">
+                <div class="rounded-lg shadow-lg bg-white max-w-sm text-center bg-teal-50">
                     <h4 class="text-gray-900 text-3xl font-medium mb-2">
                      ${phone.brand}
                      </h4>
@@ -86,10 +89,43 @@ const displayPhoneDetail = specifications => {
     // replace the previous specification to new one
     phoneDetail.textContent = '';
 
+    let release = '';
+    let wlan = "";
+    let bluetooth = "";
+    let gps = "";
+    let nfc = "";
+    let radio = "";
+    let usb = "";
+
+    if (specifications.releaseDate == '') {
+        release = 'Not Found';
+    }
+    else {
+        release = specifications.releaseDate;
+    }
+
+    let other = specifications.others?.WLAN;
+    if (other == undefined) {
+        wlan = 'No info to show';
+        bluetooth = 'No info to show';
+        gps = 'No info to show';
+        nfc = 'No info to show';
+        radio = 'No info to show';
+        usb = 'No info to show';
+    }
+    else {
+        wlan = specifications.others.WLAN;
+        bluetooth = specifications.others.Bluetooth;
+        gps = specifications.others.GPS;
+        nfc = specifications.others.NFC;
+        radio = specifications.others.Radio;
+        usb = specifications.others.USB;
+    }
+
     const div = document.createElement('div');
     div.innerHTML = `
 
-       <div class="flex flex-col md:flex-row md:max-w-full rounded-lg bg-white shadow-lg bg-teal-100 ">
+       <div class="flex flex-col md:flex-row md:max-w-full rounded-lg bg-white shadow-lg bg-teal-50 ">
                    
                <img class="w-full"
                    src="${specifications.image}" alt="" />
@@ -98,7 +134,7 @@ const displayPhoneDetail = specifications => {
                        Brand: ${specifications.brand}
                     </h4>
                    <h5 class="text-gray-900 text-xl font-medium mb-2">Name: ${specifications.name}</h5>
-                   <p class="text-gray-900 text-sm font-medium mb-2">Release Date: ${specifications.releaseDate}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">Release Date: ${release}</p>
                      <h5 class="text-gray-900 text-xl font-medium mb-2">Main Features </h5>
                    <p class="text-gray-900 text-sm font-medium mb-2">Chipset: ${specifications.mainFeatures.chipSet}</p>
                    <p class="text-gray-900 text-sm font-medium mb-2">Display Size: ${specifications.mainFeatures.displaySize}</p>
@@ -107,12 +143,12 @@ const displayPhoneDetail = specifications => {
                     <h5 class="text-gray-900 text-xl font-medium mb-2">Sensors</h5>
                    <p class="text-gray-900 text-sm font-medium mb-2"> ${specifications.mainFeatures.sensors}</p>
                     <h5 class="text-gray-900 text-xl font-medium mb-2">Others </h5>
-                   <p class="text-gray-900 text-sm font-medium mb-2">Bluetooth: ${specifications.others?.Bluetooth}</p>
-                   <p class="text-gray-900 text-sm font-medium mb-2">GPS: ${specifications.others?.GPS}</p>
-                   <p class="text-gray-900 text-sm font-medium mb-2">NFC: ${specifications.others?.NFC}</p>
-                   <p class="text-gray-900 text-sm font-medium mb-2">Radio: ${specifications.others?.Radio}</p>
-                   <p class="text-gray-900 text-sm font-medium mb-2">USB: ${specifications.others?.USB}</p>
-                   <p class="text-gray-900 text-sm font-medium mb-2">WLAN: ${specifications.others?.WLAN}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">Bluetooth: ${bluetooth}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">GPS: ${gps}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">NFC: ${nfc}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">Radio: ${radio}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">USB: ${usb}</p>
+                   <p class="text-gray-900 text-sm font-medium mb-2">WLAN: ${wlan}</p>
 
                </div>
            </div>`;
